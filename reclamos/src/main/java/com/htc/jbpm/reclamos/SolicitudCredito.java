23f4446567207b53b6065e2910965119aaf2e6ac
krisv <>
2015-09-09 10:19:39 -0600

    {/reclamos/src/main/java/com/htc/jbpm/reclamos/SolicitudCredito.java}

--- reclamos/src/main/java/com/htc/jbpm/reclamos/SolicitudCredito.java
+++ reclamos/src/main/java/com/htc/jbpm/reclamos/SolicitudCredito.java
@@ -22,8 +22,6 @@ public class SolicitudCredito implements java.io.Serializable
 
    private java.lang.String cobroAplicado;
 
-   private java.lang.String reclamoGestionado;
-
    private java.lang.String documentacionCorrecta;
 
    public SolicitudCredito()
@@ -90,16 +88,6 @@ public class SolicitudCredito implements java.io.Serializable
       this.cobroAplicado = cobroAplicado;
    }
 
-   public java.lang.String getReclamoGestionado()
-   {
-      return this.reclamoGestionado;
-   }
-
-   public void setReclamoGestionado(java.lang.String reclamoGestionado)
-   {
-      this.reclamoGestionado = reclamoGestionado;
-   }
-
    public java.lang.String getDocumentacionCorrecta()
    {
       return this.documentacionCorrecta;
@@ -113,8 +101,7 @@ public class SolicitudCredito implements java.io.Serializable
    public SolicitudCredito(java.lang.String nombreSuperior,
          java.lang.String solicitanteCredito, java.lang.String numeroCuenta,
          java.math.BigDecimal monto, java.lang.String concepto,
-         java.lang.String cobroAplicado, java.lang.String reclamoGestionado,
-         java.lang.String documentacionCorrecta)
+         java.lang.String cobroAplicado, java.lang.String documentacionCorrecta)
    {
       this.nombreSuperior = nombreSuperior;
       this.solicitanteCredito = solicitanteCredito;
@@ -122,7 +109,6 @@ public class SolicitudCredito implements java.io.Serializable
       this.monto = monto;
       this.concepto = concepto;
       this.cobroAplicado = cobroAplicado;
-      this.reclamoGestionado = reclamoGestionado;
       this.documentacionCorrecta = documentacionCorrecta;
    }
 
